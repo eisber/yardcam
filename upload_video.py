15c3a6df4aa26e8c85a9d4935bf5a476c9d64aba
Markus Cozowicz <eisber@eisber.net>
2021-12-17 18:32:59 +0000

    patch for new versions

--- upload_video.py
+++ upload_video.py
@@ -2,90 +2,93 @@ import os
 import time
 # make sure we don't overlap when called multiple times in crontab
 LOCK_FILE = "/tmp/cam-video.pid"
+from filelock import Timeout, FileLock
 
 # lock = FileLock("/tmp/cam-video.lock", timeout=600)
 # with lock:
-if os.path.exists(LOCK_FILE):
-    print("old process running")
-    last_modified = os.path.getmtime(LOCK_FILE)
-    now = time.localtime()
+# if os.path.exists(LOCK_FILE):
+    # print("old process running")
+    # last_modified = os.path.getmtime(LOCK_FILE)
+    # now = time.time()
 
-    if now - last_modified > 60*10:
-        print("very old process running")
+    # if now - last_modified > 60*10:
+        # print("very old process running")
         # TODO: kill old process
 
-    exit(0)
+    # exit(0)
 
-else:
-    with open(LOCK_FILE, "w") as f:
-        f.write(str(os.getpid()))
+# else:
+    # with open(LOCK_FILE, "w") as f:
+        # f.write(str(os.getpid()))
+
+lock = FileLock("/tmp/cam-video.lock", timeout=600)
+with lock:
+        from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+        from filelock import Timeout, FileLock
+        import re
+        import requests
+        import xml.etree.ElementTree as ET
+        import tqdm
+        from datetime import datetime 
+        from requests.auth import HTTPDigestAuth
+        from dotenv import load_dotenv
+
+        # load .env
+        # dotenv.load()
+        load_dotenv()
+
+        AZURE_STORAGE_CONNECTION_STRING = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
+        CONTAINER_NAME = os.environ.get("CONTAINER_NAME")
+        HOSTNAME = os.environ.get("HOSTNAME")
+        USERNAME = os.environ.get("USERNAME")
+        PASSWORD = os.environ.get("PASSWORD")
+
+        MAX_RESULT = os.environ.get("MAX_RESULT")
 
 
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
-from filelock import Timeout, FileLock
-import re
-import requests
-import xml.etree.ElementTree as ET
-import tqdm
-from datetime import datetime 
-from requests.auth import HTTPDigestAuth
-import dotenv
 
-# load .env
-dotenv.load()
+        try:
+            print(f"{datetime.now()} Get recordings...")
+            r = requests.get(
+                    f"http://{HOSTNAME}/axis-cgi/record/list.cgi?maxnumberofresults={MAX_RESULT}&startatresultnumber=0&recordingid=all&sortorder=descending",
+                    auth=HTTPDigestAuth(USERNAME, PASSWORD))
 
-AZURE_STORAGE_CONNECTION_STRING = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
-CONTAINER_NAME = os.environ.get("CONTAINER_NAME")
-HOSTNAME = os.environ.get("HOSTNAME")
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
+            blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
-MAX_RESULT = os.environ.get("MAX_RESULT")
+            xml_doc = ET.fromstring(r.text)
+            t = tqdm.tqdm(list(xml_doc.iter('recording')))
+            for recording in t:
+                id = recording.get("recordingid")
 
+                # Parse filename
+                m = re.search("(\d{4})(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)", id)
+                s = "/".join([m.group(i) for i in range(1, 5)])
+                filename = f"data/{s}-{m.group(5)}-{m.group(6)}-00.mkv"
 
+                # Get Blob endpoint
+                blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
 
-try:
-    print(f"{datetime.now()} Get recordings...")
-    r = requests.get(
-            f"http://{HOSTNAME}/axis-cgi/record/list.cgi?maxnumberofresults={MAX_RESULT}&startatresultnumber=0&recordingid=all&sortorder=descending",
-            auth=HTTPDigestAuth(USERNAME, PASSWORD))
+                # Don't upload twice
+                if not blob_client.exists():
+                    t.set_description(f"Transferring {filename}...")
 
-    blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
+                    # Stream copy the video
+                    with requests.get(
+                            f"http://{HOSTNAME}/axis-cgi/record/download.cgi?recordingid={id}",
+                            stream=True,
+                            auth=HTTPDigestAuth(USERNAME, PASSWORD)) as video_req:
 
-    xml_doc = ET.fromstring(r.text)
-    t = tqdm.tqdm(list(xml_doc.iter('recording')))
-    for recording in t:
-        id = recording.get("recordingid")
+                        video_req.raise_for_status()
 
-        # Parse filename
-        m = re.search("(\d{4})(\d\d)(\d\d)_(\d\d)(\d\d)(\d\d)", id)
-        s = "/".join([m.group(i) for i in range(1, 5)])
-        filename = f"data/{s}-{m.group(5)}-{m.group(6)}-00.mkv"
+                        # upload chunks of 4MB
+                        blob_id = 0
+                        for chunk in video_req.iter_content(chunk_size=4*1024*1024):
 
-        # Get Blob endpoint
-        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=filename)
+                            blob_client.stage_block(str(blob_id).zfill(6), chunk)
 
-        # Don't upload twice
-        if not blob_client.exists():
-            t.set_description(f"Transferring {filename}...")
+                            blob_id += 1
 
-            # Stream copy the video
-            with requests.get(
-                    f"http://{HOSTNAME}/axis-cgi/record/download.cgi?recordingid={id}",
-                    stream=True,
-                    auth=HTTPDigestAuth(USERNAME, PASSWORD)) as video_req:
-
-                video_req.raise_for_status()
-
-                # upload chunks of 4MB
-                blob_id = 0
-                for chunk in video_req.iter_content(chunk_size=4*1024*1024):
-
-                    blob_client.stage_block(str(blob_id).zfill(6), chunk)
-
-                    blob_id += 1
-
-                blob_client.commit_block_list(
-                        [str(i).zfill(6) for i in range(blob_id)])
-finally:
-    os.remove(LOCK_FILE)
+                        blob_client.commit_block_list(
+                                [str(i).zfill(6) for i in range(blob_id)])
+        finally:
+            os.remove(LOCK_FILE)
